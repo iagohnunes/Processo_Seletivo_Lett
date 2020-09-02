ad1bc57c73e263d7da7edd188f8388de3b2e31fc
Iago Henrique <48492576+iagohnunes@users.noreply.github.com>
2020-09-02 18:16:22 -0300

    Update Voo.java

--- DesafioPoo/src/com/test1_POO/model/Voo.java
+++ DesafioPoo/src/com/test1_POO/model/Voo.java
@@ -1,5 +1,17 @@
 package com.test1_POO.model;
 
+/*
+**********************************************************
+Programa: DesafioPoo
+Autor: Iago H. Nunes
+Criado em 02/09/2020
+Descricao:
+  Essa interface sera implementa pelos animais que possam 
+voar, assim sendo necessário a criação do método que define
+a altura de voo desse animais.
+***********************************************************
+*/
+
 public interface Voo {
     void setAltitudeDeVoo(double altitude);
 }
