4061630bef82344fd7cf53f25054d95d226ac418
Iago Henrique <48492576+iagohnunes@users.noreply.github.com>
2020-09-02 18:08:02 -0300

    Update Mamiferos.java

--- DesafioPoo/src/com/test1_POO/model/Mamiferos.java
+++ DesafioPoo/src/com/test1_POO/model/Mamiferos.java
@@ -1,5 +1,19 @@
 package com.test1_POO.model;
 
+/*
+**********************************************************
+Programa: DesafioPoo
+Autor: Iago H. Nunes
+Criado em 02/09/2020
+Descricao:
+  Essa classe extende os atributos de Animal e implementa 
+os atributos necessários que os mamiferos da fazenda precisa para
+ser registradas no sistema.
+  junto aos atributos, também e definida as ações que 
+apenas os mamiferos são capazes de realizar, como amamentar.
+***********************************************************
+*/
+
 public abstract class Mamiferos extends Animal{
     protected String corDoPelo;
 
