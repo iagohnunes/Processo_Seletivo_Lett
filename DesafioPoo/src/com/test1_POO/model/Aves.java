9dbfd0e5ad0406983864790859a19a57bb707bf5
Iago Henrique <48492576+iagohnunes@users.noreply.github.com>
2020-09-02 18:06:34 -0300

    Update Aves.java

--- DesafioPoo/src/com/test1_POO/model/Aves.java
+++ DesafioPoo/src/com/test1_POO/model/Aves.java
@@ -1,5 +1,19 @@
 package com.test1_POO.model;
 
+/*
+**********************************************************
+Programa: DesafioPoo
+Autor: Iago H. Nunes
+Criado em 02/09/2020
+Descricao:
+  Essa classe extende os atributos de Animal e implementa 
+os atributos necessários que as aves da fazenda precisa para
+ser registradas no sistema.
+  junto aos atributos, também e definida as ações que 
+apenas as aves são capazes de realizar.
+***********************************************************
+*/
+
 public abstract class Aves extends Animal {
     protected String corDasPenas;
 
